b37619dedf73eac2c7a8c2a18c85bab8b31d9249
MaximKhrulev <85082206+MaximKhrulev@users.noreply.github.com>
2021-05-31 10:03:21 +1200

    Add files via upload

---
+++ summative code 1 max/index.js
@@ -0,0 +1,10 @@
+function toggleNav() {
+    var x = document.getElementById("topNav");
+    if (x.className === "top-nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "top-nav";
+    }
+  }
+  
+  
